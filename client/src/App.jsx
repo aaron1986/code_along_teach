f2825b7c6178e90768dbfefc31062cb29c9cb989
aaron1986 <aaronbrucesmith86@gmail.com>
2023-11-02 19:46:57 +0000

    late I know...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,70 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
+
 function App() {
+  //store our from and to languages
+  const [from, setForm] = useState("en");
+  const [to, setTo] = useState("es");
+
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState({});
+
+
+  //unsplash data
+  const [imageURL, setImageURL] = useState('');
+
+   // Handle translation form submit
+   async function handleTranslate(event) {
+    event.preventDefault();
+    const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
+    try {
+      const res = await axios.get(API);
+      setTranslation(res.data);
+      setImageURL(res.data.image);
+      console.log(res.data);
+    } catch (error) {
+      console.error('Error translating:', error);
+    }
+  }
+
 
   return (
     <>
-    <h1>Working app.jsx</h1>
-    </>
+      <form onSubmit={handleTranslate}>
+        <div className="container">
+          <select onChange={(event) => setForm(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <input placeholder="Translate" onChange={(event) => setWord(event.target.value)}/>
+        </div>
+
+        <div className="container">
+          <select onChange={(event) => setTo(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <div className='output'>
+            {translation.translation}
+        </div>
+        </div>
+        <button>Submit</button>
+      </form>
+      
+        <div>
+          <h2>Image:</h2>
+          <img src={imageURL} alt="Image" />
+        </div>
+    
+      </>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -11,6 +11,27 @@ app.use(cors());
 app.get("/", (request, response) => {
     response.json("Its Working!!");
   });
+  app.get("/translate", async (request, response) => {
+    const { word, from, to } = request.query;
+  
+    // make our API call
+    const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+    const res = await axios.get(API);
+
+    //unsplash api
+    const sploosh_api = `https://api.unsplash.com/search/photos?page=1&query=${word}&client_id=njF7szjcV9ReHwyPn9rSpsiKdG6p9IgOrlm_SOi8YHk`;
+    const ans = await axios.get(sploosh_api);
+    //retrieve description data from api
+    //response.json(ans.data.results[0].urls.full);
+  
+    const wrangledData = {
+      translation: res.data.responseData.translatedText,
+      match: res.data.responseData.match,
+      image: ans.data.results[0].urls.full,
+    };
+  
+    response.json(wrangledData);
+  });
 
 //leave alone
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
